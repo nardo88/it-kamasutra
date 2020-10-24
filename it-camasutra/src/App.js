d07eea263366416abd8d9d27a2656597b8a4bdf1
nardo88 <nardo1988@mail.ru>
2020-10-25 00:40:02 +0300

    new

--- it-camasutra/src/App.js
+++ it-camasutra/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react'
+
 import './App.css'
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Profile from './components/Profile';
 function App() {
   return (
     <div className="app-wrapper">
-        <header className="header">
-          <img className="img" src="https://i.pinimg.com/originals/3f/3d/d9/3f3dd9219f7bb1c9617cf4f154b70383.jpg" alt=""/>
-        </header>
-        <nav className="nav">
-          <ul>
-            <li><a href="#!">Profile</a></li>
-            <li><a href="#!">Messages</a></li>
-            <li><a href="#!">News</a></li>
-            <li><a href="#!">Music</a></li>
-          </ul>
-          <a href="#!">Settings</a>
-        </nav>
-        <div className="content">
-          <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" alt=""/>
-        </div>
+        <Header />
+        <NavBar />
+        <Profile />
     </div>
   );
 }
---
+++ it-camasutra/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const NavBar = () => {
+    return (
+        <nav className="nav">
+          <ul>
+            <li><a href="#!">Profile</a></li>
+            <li><a href="#!">Messages</a></li>
+            <li><a href="#!">News</a></li>
+            <li><a href="#!">Music</a></li>
+          </ul>
+          <a href="#!">Settings</a>
+        </nav>
+    )
+}
+
+export default NavBar
