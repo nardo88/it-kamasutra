6a3f34c87bbf09aef79d8879abc61df454fbb00d
nardo88 <nardo1988@mail.ru>
2020-11-02 13:35:21 +0300

    ООП

--- it-camasutra/src/index.js
+++ it-camasutra/src/index.js
@@ -1,12 +1,23 @@
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import store  from './redux/state'
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state'
-import { renderDom } from './render.js'
 
-renderDom(state)
+export const renderDom = state => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App state={store.state} addPost={store.addPost.bind(store)} addPostText={store.addPostText.bind(store)} />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  }
 
+renderDom(store.state)
+
+
+store.subscribe(renderDom)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- it-camasutra/src/redux/state.js
+++ it-camasutra/src/redux/state.js
@@ -1,49 +1,65 @@
-import { renderDom } from '../render.js'
 
 
-let state = {
-    profilePage: {
-        postsData: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 12},
-            {id: 2, message: 'it is my first post', likesCount: 11},
-            {id: 3, message: "I'm back", likesCount: 8},
-            {id: 3, message: "I'm back", likesCount: 8},
-        ],
-        newPostText: "default"
+const store = {
+    // Сам state
+    state : {
+        // страница с постами
+        profilePage: {
+            // посты
+            postsData: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'it is my first post', likesCount: 11},
+                {id: 3, message: "I'm back", likesCount: 8},
+                {id: 3, message: "I'm back", likesCount: 8},
+            ],
+            // переменная для изменения текста в textarea
+            newPostText: "default"
+        },
+        // страница с сообщениями
+        messagesPage: {
+            // список с кем переписываемся
+            dialogData: [
+                { id: 1, name: 'Дмитрий' },
+                { id: 2, name: 'Ольга' },
+                { id: 3, name: 'Максим' },
+                { id: 4, name: 'Мария' },
+                { id: 5, name: 'Александр' },
+            ],
+            // список сообщений
+            messagesData: [
+                { id: 1, message: 'Hi!' },
+                { id: 2, message: 'How are you?' },
+                { id: 3, message: 'i want to sleep' },
+                { id: 4, message: 'By!' },
+            ],
+        }
     },
-    messagesPage: {
-        dialogData: [
-            { id: 1, name: 'Дмитрий' },
-            { id: 2, name: 'Ольга' },
-            { id: 3, name: 'Максим' },
-            { id: 4, name: 'Мария' },
-            { id: 5, name: 'Александр' },
-        ],
-        
-        messagesData: [
-            { id: 1, message: 'Hi!' },
-            { id: 2, message: 'How are you?' },
-            { id: 3, message: 'i want to sleep' },
-            { id: 4, message: 'By!' },
-        ],
+    // Метод добавления поста
+    addPost(message) {
+        // создаем новый пост
+        let post = {
+            id: 5,
+            message: message,
+            likesCount: 0
+        }
+        //  пушим его в state
+        this.state.profilePage.postsData.push(post)
+        // обновляем страницу (ререндер)
+        this.callSubscriber(this.state)
+    },
+
+    // метод изменения textarea FLUX в постах
+    addPostText (text){
+        this.state.profilePage.newPostText = text
+        this.callSubscriber(this.state)
+    },
+    // функция затычка
+    callSubscriber(){
+        console.log(this);
+    },
+    subscribe(observer){
+        this.callSubscriber = observer
     }
 }
 
-export const addPost = (message) => {
-    let post = {
-        id: 5,
-        message: message,
-        likesCount: 0
-    }
-
-    state.profilePage.postsData.push(post)
-    renderDom(state)
-}
-
-export const addPostText = (text) => {
-
-    state.profilePage.newPostText = text
-    renderDom(state)
-}
-
-export default state
+export default store
