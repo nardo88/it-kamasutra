003409ffebd668aed0cdf241b32cbd668e85a070
nardo88 <nardo1988@mail.ru>
2020-12-21 16:46:32 +0300

    1

--- it-camasutra/src/components/profile/ProfilePage/ProfilePage.jsx
+++ it-camasutra/src/components/profile/ProfilePage/ProfilePage.jsx
@@ -7,7 +7,7 @@ import ProfileStatusWithHooks from './ProfileStatusWithHooks'
 
 
 const ProfilePage = (props) => {
-    
+    console.log(props);
     if(!props.profile){
         return <Preloader />
     } 
@@ -35,9 +35,10 @@ const ProfilePage = (props) => {
                 </div>
                 <div className={styles.profileInfo}>
                     <ul className={styles.listInfo}>
-                        <li className={styles.name}> <span className={styles.atributeName}>Имя: </span> {props.profile.fullName}</li>
-                        <li className={styles.status}> <ProfileStatusWithHooks status={props.status} changeStatus={props.changeStatus} /></li>
+                        <li className={styles.name}> <span className={styles.atributeName}>name: </span> {props.profile.fullName}</li>
+                        <li className={styles.status}><span>status</span> <ProfileStatusWithHooks status={props.status} changeStatus={props.changeStatus} /></li>
                         <li className={styles.job} title={props.profile.lookingForAJobDescription}> {props.profile.lookingForAJob ? `look for a job` : `I don't need a JOB`} </li>
+                        <li className={styles.info}> <span className={styles.atributeName}>about: </span> {props.profile.aboutMe ? props.profile.aboutMe : 'Не указано'}</li>
                     </ul>
                 </div>
             </div>
