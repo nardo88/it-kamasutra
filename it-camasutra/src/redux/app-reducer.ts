61f1827e44be5bf65dfb0950a9703b45ad66ada0
nardo88 <nardo1988@mail.ru>
2021-07-06 11:38:49 +0300

    1

--- it-camasutra/src/redux/app-reducer.ts
+++ it-camasutra/src/redux/app-reducer.ts
@@ -2,12 +2,20 @@ import { setAuthDataThunkCreator} from './reducer-auth'
 
 // константа для типа action
 const SET_INITIALIZED = 'SET_INITIALIZED'
+
+// type для дефолтного state
+export type InitialStateType = {
+    initialized: boolean
+} 
+
+
 // state для инициализации
-const initialState = {
+const initialState: InitialStateType  = {
     initialized: false
 } 
+
 // тело reducer
-const appReducer = (state = initialState, action) => {
+const appReducer = (state = initialState, action: SetInitializedActionType): InitialStateType => {
     switch (action.type){
         case SET_INITIALIZED: {
             // возвращаем объект и меняем значение initialized на true
@@ -21,19 +29,21 @@ const appReducer = (state = initialState, action) => {
     }
 }
 
-// action creator
-export const setInitialized = () => {
-    return {
-        type: SET_INITIALIZED,
-       
-        }
+
+// type для action
+
+type SetInitializedActionType = {
+    type: typeof SET_INITIALIZED
 }
+
+// action creator
+export const setInitialized = (): SetInitializedActionType =>  ({type: SET_INITIALIZED})
 
 
 // создание thunk -------------------------------------------------------
 
 // thunk creator
-export const initializeApp = () => (dispatch) => {
+export const initializeApp = () => (dispatch: any) => {
     // диспатчим санку которая делает запрос на сервер
     // проверяет авторизованы ли мы и которая после этого в state 
     // диспатчит данные авторизованного пользователя (id, login и т.д.)
